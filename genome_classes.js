061ec32377aa81d693c543d1555305e993004341
Sultan Nazir <sultan@intron.bio.uu.nl>
2025-05-16 17:03:41 +0200

    genome model first draft

--- genome_classes.js
+++ genome_classes.js
@@ -4,8 +4,9 @@ class Genome {
     this.uid = genomeIds.next()
     //initialise a genome with init_nc many non-coding segments and one attachment region
     this.chromosome = []
-    this.chromosome.push(new Gene("att"))
+    for (let i = 0; i < init_att; i++)this.chromosome.push(new Gene("att"))
     for (let i = 0; i < init_nc; i++) this.chromosome.push(new Gene("."))
+    this.first_lock = this.chromosome[0].lock
     this.dupSites = Array(this.chromosome.length).fill(0)
     this.phages = []
     this.genome_length = this.chromosome.length
@@ -23,6 +24,7 @@ class Genome {
       child.phages.push([])
       for (let j = 0; j < this.phages[i].length; j++) child.phages[i].push(this.phages[i][j].copy())
     }
+    child.first_lock = this.first_lock
     child.genome_length = this.genome_length
     child.dupSites = this.dupSites
     child.fitness = this.fitness
@@ -36,9 +38,15 @@ class Genome {
   calculate_fitness() {
     this.susceptible = 0
     for (let i = 0; i < this.chromosome.length; i++){
-      if (this.chromosome[i].type == "att") this.susceptible = 1
+      if (this.chromosome[i].type == "att"){
+        this.susceptible = 1
+        this.first_lock = this.chromosome[i].lock
+      }
     }
-    if (this.phages.length == 0 && this.susceptible == 0) this.fitness = 0
+    if (this.susceptible == 0) {
+      this.fitness = 0
+      this.first_lock = -1
+    }
     else{
       this.genome_length = this.chromosome.length
       this.dupSites = Array(this.chromosome.length).fill(0)
@@ -65,6 +73,7 @@ class Genome {
         mutation = true
       }
       else if (randomnr < gene_deletion_rate + gene_inactivation_rate + gene_duplication_rate) {
+        // att has a lower
         var insertion = sim.rng.genrand_int(0,this.dupSites.length-1)
         if (this.dupSites[insertion] == 0) this.chromosome.push(this.chromosome[i].copy())
         else if (this.chromosome[i].type != "att") this.phages[this.dupSites[insertion]-1].push(this.chromosome[i].copy())
@@ -109,11 +118,14 @@ class Genome {
 
 class Virion {
 
-  constructor() {
+  constructor(type, parent) {
     this.uid = virionIds.next()
     this.genome = []
-    this.genome.push(new Gene("L"))
-    this.genome.push(new Gene("R"))
+    if (type) {
+      this.genome.push(new Gene("L"))
+      this.genome.push(new Gene("R"))
+    }
+    else for (let i = 0; i < parent.length; i++) this.genome.push(parent[i].copy())
   }
 }
 
@@ -123,7 +135,10 @@ class Gene {
     this.uid = geneIds.next()
     this.type = type
     if (type == "att") this.lock = sim.rng.genrand_real1()
-    else if (type == "L") this.key = sim.rng.genrand_real1()
+    else if (type == "L") {
+      this.key = sim.rng.genrand_real1()
+      this.lysis = sim.rng.genrand_real1() * max_induction_rate
+    }
     else if (type == "R") {
       this.highX_state = 1
       this.lowX_state = 1
@@ -132,25 +147,25 @@ class Gene {
 
   copy() {
     let new_gene = new Gene(this.type)
-    new_gene.mutate()
-    return new_gene
-  }
 
-  mutate() {
     if (this.type == "att") {
-      var rnr = sim.genrand_real1()
+      var rnr = sim.rng.genrand_real1()
       if (rnr < coevolution_rate) new_gene.lock = ((this.lock + coevolution_step*(sim.rng.genrand_real1()*2-1)) + 1) % 1
       else new_gene.lock = this.lock
     }
 
     else if (this.type == "L") {
-      var rnr = sim.genrand_real1()
-      if (rnr < coevolution_rate) new_gene.key = ((this.key + coevolution_step*(sim.rng.genrand_real1()*2-1)) + 1) % 1
+      var rnr1 = sim.rng.genrand_real1()
+      if (rnr1 < coevolution_rate) new_gene.key = ((this.key + coevolution_step*(sim.rng.genrand_real1()*2-1)) + 1) % 1
       else new_gene.key = this.key
+
+      var rnr2 = sim.rng.genrand_real1()
+      if (rnr2 < lysis_mutation_rate) new_gene.lysis = sim.rng.genrand_real1() * max_induction_rate
+      else new_gene.lysis = this.lysis
     }
 
     else if (this.type == "R") {
-      var rnr = sim.genrand_real1()
+      var rnr = sim.rng.genrand_real1()
       if (rnr < 0.5*gene_regulation_mutation_rate) {
         new_gene.lowX_state = this.lowX_state
         new_gene.highX_state = 1 - this.highX_state
@@ -164,17 +179,20 @@ class Gene {
         new_gene.highX_state = this.highX_state
       }
     }
+    return new_gene
   }
 
 }
 
 function shuffle(a) {
-  var j, x, i;
-  for (i = a.length - 1; i > 0; i--) {
-    j = Math.floor(sim.rng.genrand_real2() * (i + 1));
-    x = a[i];
-    a[i] = a[j];
-    a[j] = x;
+  if (a.length > 1) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+      j = Math.floor(sim.rng.genrand_real2() * (i + 1));
+      x = a[i];
+      a[i] = a[j];
+      a[j] = x;
+    }
   }
   return a;
 }
